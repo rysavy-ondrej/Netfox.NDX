dcaa782a4187536647019cff5bf6b30d49bab0bc
Ondrej Rysavy <rysavy@fit.vutbr.cz>
2017-11-28 16:30:59 +0100

    Updating decoder extensions.

---
+++ Ndx.Decoders/Decoders/Core/PacketArp.Extensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ndx.Decoders.Core
+{
+    public partial class Arp
+    {
+        public IPAddress ArpSrcProtoIpv4Address => new IPAddress(this.ArpSrcProtoIpv4.ToByteArray());
+        public IPAddress ArpDstProtoIpv4Address => new IPAddress(this.ArpDstProtoIpv4.ToByteArray());
+        public PhysicalAddress ArpSrcHwMacAddress => new PhysicalAddress(this.ArpSrcHwMac.ToByteArray());
+        public PhysicalAddress ArpDstHwMacAddress => new PhysicalAddress(this.ArpDstHwMac.ToByteArray());
+    }
+}
---
+++ Ndx.Diagnostics/EventWindow.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ndx.Diagnostics
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class TimeWindow
+    {
+        long m_from;
+        long m_to;
+        public TimeWindow(long from, long to)
+        {
+            m_from = from;
+            m_to = to;
+        }
+
+        public bool In(long timestamp)
+        {
+            return timestamp >= m_from && timestamp <= m_to;
+        }
+
+        public (T, bool) In<T>(T value, long timestamp)
+        {
+            if (timestamp >= m_from && timestamp <= m_to)
+            {
+                return (value, true);
+            }
+            else
+            {
+                return (value, false);
+            }
+        }        
+    }
+
+}
